508578cae9580717d82878b5113f93b08cfe7814
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-09-03 21:53:30 +0000

    Fixed paths issue

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const getViewFilePath = (fileName) => path.resolve(__dirname, '../../../client/src/views', fileName);
+const getViewFilePath = (fileName) => path.resolve(__dirname, '..', '..', 'client', 'src', 'views', fileName);
 
 router.get('/login', (request, reply) => {
-    console.log(getViewFilePath('login.html'))
     reply.sendFile(getViewFilePath('login.html'));
 });
 
-router.get('/', (request, reply) => {
-    reply.send({ msg: 'Hello my lord' });
+router.get('/signup', (request, reply) => {
+    reply.sendFile(getViewFilePath('signup.html'));
 });
+
 
 module.exports = router;
