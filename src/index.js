1febff0d553d5847bdcd53b5e58f2f2105355cff
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-09-03 21:17:55 +0000

    Moved files here and there

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const http = require('http');
+const socket = require('socket.io');
+require('dotenv').config();
+const listerApp = express();
+const server = http.createServer(listerApp);
+const io = socket(server);
+
+listerApp.use(express.json());
+// listerApp.use('/api/v1', require('./routes/source-routes'));
+listerApp.use('/', require('./routes/routes'));
+
+server.listen(8080, async () => {
+    // await mongoose.connect(process.env.srv);
+    console.log(`Server is running on http://localhost:8080`);
+});
