67b1f008ac437cbd6295af8f0e7e4fa87f785f06
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-09-04 10:43:53 +0000

    Using all sorts of middlewares

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const path = require('path');
+const passport = require('passport');
 const http = require('http');
+const flash = require('express-flash');
 const socket = require('socket.io');
 require('dotenv').config();
 const listerApp = express();
@@ -11,10 +14,19 @@ const io = socket(server);
 listerApp.use(express.static(path.join(__dirname, '..', 'client', 'src')));
 
 listerApp.use(express.json());
+listerApp.use(express.urlencoded({ extended: true }));
+listerApp.use(session({
+    secret: process.env.sessionkey,
+    resave: false,
+    saveUninitialized: false,
+}));
+listerApp.use(passport.initialize());
+listerApp.use(passport.session());
+listerApp.use(flash());
 // listerApp.use('/api/v1', require('./routes/source-routes'));
 listerApp.use('/', require('./routes/routes'));
 
 server.listen(8080, async () => {
-    // await mongoose.connect(process.env.srv);
+    await mongoose.connect(process.env.srv);
     console.log(`Server is running on http://localhost:8080`);
 });
