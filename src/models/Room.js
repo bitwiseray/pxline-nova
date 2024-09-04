b3443a97b41c2ae0087511169ca4e1c10a9050bf
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-09-04 12:11:54 +0000

    Using Model methods instead

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,15 +1,36 @@
 const mongoose = require('mongoose');
 
-const rooms_schema = new mongoose.Schema({
-  title: { type: String, required: true, unique: true },
+const roomSchema = new mongoose.Schema({
+  title: { type: String, required: true },
   icon: { type: String },
-  members: { type: Array },
+  members: { type: [mongoose.Schema.Types.ObjectId], ref: 'Profile' },
   roles: { type: Object },
   settings: { type: Object },
-  createdAt: { type: Number },
+  createdAt: { type: Number, default: Date.now },
   socials: { bio: String },
-  chats: { chat_id: String, chat_type: String },
+  chats: {
+    chat_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat' },
+    chat_type: { type: String }
+  }
 });
 
-const Room = mongoose.model('rooms', rooms_schema);
+roomSchema.methods.addMember = async function (userId) {
+  if (this.members.includes(userId)) {
+    throw { code: 'HALTED', message: `User \`${userId}\` is already a member` };
+  }
+
+  try {
+    this.members.push(userId);
+    const user = await mongoose.model('Profile').findById(userId);
+    if (!user) throw new Error(`[ERROR] User ID: ${userId} not found`);
+    user.chats.push({ chat_id: this._id.toString(), chat_type: 'room' });
+    await this.save();
+    await user.save();
+    return { code: 'SUCCESS', message: `User \`${userId}\` successfully added to room` };
+  } catch (error) {
+    throw { error: error, code: 'INTERNAL_SERVER_ERROR' };
+  }
+};
+
+const Room = mongoose.model('Room', roomSchema);
 module.exports = Room;
