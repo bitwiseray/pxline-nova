22e94f64fe0aa908507b6cab07df3904ad21db9b
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-09-04 10:43:01 +0000

    Added login route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const passport = require('passport');
+const { checkAuth, checkNotAuth } = require('../config/validators');
+const initGateway = require('../config/auth');
+
 const getViewFilePath = (fileName) => path.resolve(__dirname, '..', '..', 'client', 'src', 'views', fileName);
+initGateway();
 
 router.get('/login', (request, reply) => {
     reply.sendFile(getViewFilePath('login.html'));
 });
 
+router.get('/me', (request, reply) => {
+    reply.sendFile(getViewFilePath('login.html'));
+});
+
+router.get('/', checkAuth, async (request, reply) => {
+    reply.send({ message: `Hello, ${request.user?.display_name}!` });
+});
+
+router.post('/login', checkNotAuth, (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.redirect(`/login?error=${info.message}`);
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            return res.redirect('/');
+        });
+    })(req, res, next);
+});
+
 router.get('/signup', (request, reply) => {
     reply.sendFile(getViewFilePath('signup.html'));
 });
-
 
 module.exports = router;
