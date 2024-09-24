adfe8ed7071d35eeecb8f65b71cf3aded405d70c
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-09-24 10:42:48 +0000

    doing something

---
+++ client/src/scripts/messages.js
@@ -0,0 +1,9 @@
+const socket = io('/');
+const ext = JSON.parse(localStorage.getItem('ext'));
+const { type, room, chats, extusers, user } = ext;
+
+class FactorSocketChats {
+    static getSnowfalkes () {
+        
+    }
+}
